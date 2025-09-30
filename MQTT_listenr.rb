dfde5aa4aa5f0d9cc0c7d993a4f40fbe348feff1
CAFSMM <cafs.munozmontoya@gmail.com>
2025-09-30 09:46:45 -0300

    Refactor MQTT listener to read configuration from JSON file and enhance user input for broker settings

--- MQTT_listenr.rb
+++ MQTT_listenr.rb
@@ -2,9 +2,27 @@ require 'mqtt'
 require 'json'  
 
 # MQTT connection settings as variables
-mqtt_host = 'localhost'   # IP address of the MQTT broker
-mqtt_port = 1883          # Port of the MQTT broker
-mqtt_topic = 'test/#' # Topic to subscribe to
+
+DEFAUL_MQTT_HOST = json.parse(File.read('config.json'))['mqtt']['host'] || 'localhost'
+DEFAUL_MQTT_PORT = json.parse(File.read('config.json'))['mqtt']['port'] || 1883
+DEFAUL_MQTT_TOPIC = json.parse(File.read('config.json'))['mqtt']['topic'] || 'test/#'
+
+print "Enter MQTT broker host (default: #{DEFAUL_MQTT_HOST}): "
+mqtt_host = gets.strip
+mqtt_host = DEFAUL_MQTT_HOST if mqtt_host.empty?
+
+print "Enter MQTT broker port (default: #{DEFAUL_MQTT_PORT}): "
+mqtt_port_input = gets.strip
+mqtt_port = mqtt_port_input.empty? ? DEFAUL_MQTT_PORT : mqtt_port_input.to_i
+
+print "Enter MQTT topic to subscribe to (default: #{DEFAUL_MQTT_TOPIC}): "
+mqtt_topic = gets.strip
+mqtt_topic = DEFAUL_MQTT_TOPIC if mqtt_topic.empty?
+
+alert_topic = 'gmc/alert#' # Topic to publish alerts to
+
+WS558_euid = '24e124756c404098' # Unique identifier for the WS558 device
+
 
 def listen_for_messages(host, port, topic)
   MQTT::Client.connect(host: host, port: port) do |client|
@@ -12,8 +30,20 @@ def listen_for_messages(host, port, topic)
     client.subscribe(topic)
     client.get do |received_topic, message|
       puts "Received message on topic '#{received_topic}': #{message}"
-      return message
+
+      if received_topic == alert_topic
+        # Handle alert messages
+        puts "ALERT: #{message}"
+      end
+
     end
+  end
+end
+
+def configure_WS558(host, port, alert_topic, alert_message)
+  MQTT::Client.connect(host: host, port: port) do |client|
+    client.publish(alert_topic, alert_message)
+    puts "Published alert to topic '#{alert_topic}': #{alert_message}"
   end
 end
 
